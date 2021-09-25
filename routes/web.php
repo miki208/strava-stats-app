a05c0a3b6a7caca6d40476c7220dca8e1595da70
miki208 <miloss208@gmail.com>
2021-09-25 11:09:16 +0200

    Add temporary logging

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Log;
 */
 
 $router->get(config('strava-params.webhook-callback-url'), function (Request $request) use ($router) {
-    echo $request->has('hub.mode') ? "true" : "false";
+    echo $request->has('mod') ? "true" : "false";
 
     if(!$request->has(['hub.mode', 'hub.challenge', 'hub.verify_token']))
         return response()->json([], Response::HTTP_BAD_REQUEST, [], JSON_UNESCAPED_SLASHES);
